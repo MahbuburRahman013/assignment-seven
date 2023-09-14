235bcf1be01079b0def32fb08a8e4997952fb7ca
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-09-15 00:23:57 +0600

    Three documents is formatted

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,23 +1,31 @@
-import PropTypes from 'prop-types'
-const Cart = ({titleArr,totalPrice,addHour,lessHour}) => {
-    return (
-        <div>
-            <h1 className="font-bold text-xl text-blue-500 border-b-2 pb-5">Credit Hour Remaining {lessHour} hr</h1>
-            <h1 className="font-bold text-xl py-5">Course Name</h1>
-            <div className="border-b-2 pb-5">
-                  {
-                    titleArr.map((title,index)=> <p key={index}>{index+1} {title} </p>)
-                  }
-            </div>
-            <h2 className="text-base font-semibold border-b-2 text-gray-500 py-5">Total Credit Hour : {addHour}</h2>
-            <h2 className="text-base font-semibold text-gray-500 py-5">Total Price : {totalPrice} USD</h2>
-        </div>
-    );
+import PropTypes from "prop-types";
+const Cart = ({ titleArr, totalPrice, addHour, lessHour }) => {
+  return (
+    <div>
+      <h1 className="font-bold text-xl text-blue-500 border-b-2 pb-5">
+        Credit Hour Remaining {lessHour} hr
+      </h1>
+      <h1 className="font-bold text-xl py-5">Course Name</h1>
+      <div className="border-b-2 pb-5">
+        {titleArr.map((title, index) => (
+          <p key={index}>
+            {index + 1} {title}{" "}
+          </p>
+        ))}
+      </div>
+      <h2 className="text-base font-semibold border-b-2 text-gray-500 py-5">
+        Total Credit Hour : {addHour}
+      </h2>
+      <h2 className="text-base font-semibold text-gray-500 py-5">
+        Total Price : {totalPrice} USD
+      </h2>
+    </div>
+  );
 };
 Cart.propTypes = {
-    titleArr: PropTypes.array.isRequired,
-    totalPrice: PropTypes.number.isRequired,
-    addHour: PropTypes.number.isRequired,
-    lessHour: PropTypes.number.isRequired,
-}
+  titleArr: PropTypes.array.isRequired,
+  totalPrice: PropTypes.number.isRequired,
+  addHour: PropTypes.number.isRequired,
+  lessHour: PropTypes.number.isRequired,
+};
 export default Cart;
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,52 +1,56 @@
 import { useEffect, useState } from "react";
 import Cards from "./Cards";
 import Cart from "./Cart";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Container = () => {
-   const [cards,setCards] = useState([]);
-   const [titleArr,setTitleArr] = useState([]);
-   const [totalPrice,setTotalPrice] = useState(0);
-   const [addHour,setAddHour] = useState(0);
-   const [lessHour,setLessHour] = useState(20);
+  const [cards, setCards] = useState([]);
+  const [titleArr, setTitleArr] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [addHour, setAddHour] = useState(0);
+  const [lessHour, setLessHour] = useState(20);
 
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCards(data));
+  }, []);
 
-useEffect(()=>{
-    fetch('data.json')
-    .then(res=> res.json())
-    .then(data=> setCards(data));
-},[])
+  const addTitle = (data) => {
+    const newHour = addHour + data.time_hour;
+    if (!titleArr.includes(data.title) && newHour <= 20) {
+      setTitleArr([...titleArr, data.title]);
 
-const addTitle = (data) =>{
-    const newHour = addHour + data.time_hour;  
-        if(!titleArr.includes(data.title) && newHour <= 20){
-            setTitleArr([...titleArr,data.title]);
-            
-            setTotalPrice(data.fee + totalPrice);
-          
-            setAddHour(newHour);
-            setLessHour(lessHour - data.time_hour )
+      setTotalPrice(data.fee + totalPrice);
 
-        }else{
-            toast(`You have already added "${data.title}" or "Total Credit Hour" is over!!`)
-        }    
-}
+      setAddHour(newHour);
+      setLessHour(lessHour - data.time_hour);
+    } else {
+      toast(
+        `You have already added "${data.title}" or "Total Credit Hour" is over!!`
+      );
+    }
+  };
 
-    return (
-       <div className="flex gap-x-8">
-        
-         <div className="grid grid-cols-3 w-[75%] gap-5">
-            {
-                cards.map((card,index)=> <Cards key={index} card={card} addTitle={addTitle}></Cards>)
-            }
-        </div>
-        <div className="w-[25%] bg-white rounded-lg px-4 py-5 h-[70vh] overflow-auto">
-        <Cart titleArr={titleArr} totalPrice={totalPrice} addHour={addHour} lessHour={lessHour}></Cart>
+  return (
+    <div className="flex gap-x-8">
+      <div className="grid grid-cols-3 w-[75%] gap-5">
+        {cards.map((card, index) => (
+          <Cards key={index} card={card} addTitle={addTitle}></Cards>
+        ))}
+      </div>
+      <div className="w-[25%] bg-white rounded-lg px-4 py-5 h-[70vh] overflow-auto">
+        <Cart
+          titleArr={titleArr}
+          totalPrice={totalPrice}
+          addHour={addHour}
+          lessHour={lessHour}
+        ></Cart>
         <ToastContainer></ToastContainer>
-        </div>
-       </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Container;
