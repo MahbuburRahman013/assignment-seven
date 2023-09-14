2a23b1899ce703e5d3af910a16f202b53a8150ae
Mahbubur-Rahman-1 <mahbub01304@gmail.com>
2023-09-14 20:46:51 +0600

    added three components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Container from "./components/Container";
+
 const App = () => {
   return (
-    <div>
-      
+    <div className="container mx-auto my-10">
+      <h1 className="text-5xl text-center font-bold">Course Registration</h1>
+      <div className="mt-12">
+       <Container></Container>
+      </div>
     </div>
   );
 };
